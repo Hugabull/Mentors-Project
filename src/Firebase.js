b6aad5d0a5980f3830f4d722abd89c9be7052fbf
Marshall Dunn <marshall506@icloud.com>
2024-07-15 16:31:42 -0300

    added dependintcies

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,6 +2,7 @@
   // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getAnalytics(app);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
   
   function fetchTexts() {
     db.collection("messages").orderBy("timestamp").onSnapshot((querySnapshot) => {
